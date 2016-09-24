b3e7bf076e99aed2317456f726ba8c7961fb2220
sparksjj <e.gursky@mobiles.by>
2016-09-24 21:04:22 +0300

    add settings logick

--- angular/app.js
+++ angular/app.js
@@ -11,6 +11,7 @@ var app = angular.module("myApp", ['ngRoute', 'mainPageModule']);
           });
   })
 
+
   .controller('appCtrl', ['$scope', "$rootScope", "$timeout",
     function($scope, $rootScope, $timeout) {
 
@@ -122,215 +123,39 @@ $('.purple_btn').click(function () {
 });
 
 
-
-// $('.arrow_left').click (function() {
-    
-    
-//     initDataSlide();
-// });
-
-// $('.arrow_right').click (function() {
-    
-    // initDataSlide();
-// });
-
-
-$('.arrow_right').click(function () {
-    if(!noClick) {
-        return;
-    }
-
-    if (taksaStep == 4) {
-        return;
-    }
-    taksaStep++;
-    taksa = taksaStep;
-
-    noClick = false;
-    if (($('.point_top').position().left) > (parseInt($('.slide_2_wrapper_item').css('width')) - 50)) {
-        var width = parseInt($('.slide_2_wrapper_item').css('width'));
-        $('.point_top').position().left = width;
-    } else {
-
-        var width = parseInt($('.slide_2_wrapper_item').css('width'));
-
-        var p = $('.point_top');
-        var offset = p.position().left;
-
-        var left = offset + width / 3;
-        p.css({
-            left: left,
-            transition: '1s'
-        });
-    }
-
-    
-    initDataSlide();
-
-    setTimeout(function () {
-        noClick = true;
-    }, 1000);
-});
-
-var noClick = true;
-
-
-$('.arrow_left').click(function () {
-    if(!noClick) {
-        return;
-    }
-    if (taksaStep == 1) {
-        return;
-    }
-    taksaStep--;
-    taksa = taksaStep;
-
-    noClick = false;
-
-    if (($('.point_top').position().left) < 50) {
-        var width = parseInt($('.slide_2_wrapper_item').css('width'));
-
-        $('.point_top').position().left = 0;
-    } else {
-
-        var width = parseInt($('.slide_2_wrapper_item').css('width'));
-
-        var p = $('.point_top');
-        var offset = p.position().left;
-        var left = offset - width / 3;
-        p.css({
-            left: left,
-            transition: '1s'
-        });
-    }
-
-    
-
-        // offset = Math.round($('.box .box_settings .slide_2_wrapper_item .point_top').position().left);
-        // length = $('.slide_2_wrapper_item');
-
-        // if (offset === 0) {
-        //     taksa = 1;
-        // } else if (offset === Math.round(length.width() / 3)) {
-        //     taksa = 2;
-        // } else if (offset === Math.round(length.width() / 3 * 2)) {
-        //     taksa = 3;
-        // } else if (offset === Math.round(length.width())) {
-        //     taksa = 4;
-        // }
-        initDataSlide();
-    setTimeout(function () {    
-        noClick = true;
-        
-    }, 1000);
-    
-
-});
-
 $('.point_first, .point_second, .point_third, .point_fourth').click(function () {
-    if(!noClick) {
-        return;
-    }
-    
-
-    noClick = false;
-
-     
-        var pos_left = $(this).position().left;
-        $('.point_top').css({
-            left: pos_left,
-            transition: '1s'
-
-        });
-    setTimeout(function () {
-        noClick = true;
-
-    }, 1000);
+ 
+    var pos_left = $(this).position().left;
+    $('.point_top').css({
+        left: pos_left,
+        transition: '1s'
+    });
     
 })
 
-$('.point_first').click(function() {
-    taksa = 1;
-
-    initDataSlide();
-});
-
-$('.point_second').click(function() {
-    taksa = 2;
-    initDataSlide();
-});
-
-$('.point_third').click(function() {
-    taksa = 3;
-    initDataSlide();
-});
-
-$('.point_fourth').click(function() {
-    taksa = 4;
-    initDataSlide();
-});
-
-
-
-
-/////////////////////////////////
-//блок кода требует оптимизации//
-
-
-$('.point_left_1').click(function () {
-    var pos_left = $(this).position().left;
-    $('.reserve_1').css({
-        left: pos_left,
-        transition: '1s'
-    });
-    
-    if (percentReserve1) {
-        updatePercentValues(0.5);
-    }
-    
-    
-    percentReserve1 = false;
-
-});
 
 $('.point_left_2').click(function () {
     var pos_left = $(this).position().left;
-    $('.reserve_1').css({
+    $('.reserve_2').css({
         left: pos_left,
         transition: '1s'
     });
-    
-    if (!percentReserve1) {
-        updatePercentValues(-0.5);
-    }
-    percentReserve1 = true;
 });
 
 $('.point_left_3').click(function () {
     var pos_left = $(this).position().left;
-    $('.reserve_2').css({
+    $('.reserve_3').css({
         left: pos_left,
         transition: '1s'
     });
-
-    if (percentReserve2) {
-        updatePercentValues(0.5);
-    }
-    
-    
-    percentReserve2 = false;
 });
 
 $('.point_left_4').click(function () {
     var pos_left = $(this).position().left;
-    $('.reserve_2').css({
+    $('.reserve_4').css({
         left: pos_left,
         transition: '1s'
     });
-    if (!percentReserve2) {
-        updatePercentValues(-0.5);
-    }
-    percentReserve2 = true;
 });
 
 
@@ -415,72 +240,24 @@ $('.settings_btn').click(function () {
         left: left,
         transition: '.2s'
     });
-    updateSliderButtons();
+/*    updateSliderButtons();*/
 
 })
 
 
 //  Шестерёнки
 $('.screw_1').click(function () {
-    taksa = 1;
     scrollBlockToLeft();
-
-    var width = parseInt($('.slide_2_wrapper_item').css('width'));
-
-    var p = $('.point_top');
-    var offset = p.position().left;
-
-    var left = 0;
-    p.css({
-        left: left,
-        transition: '.2s'
-    });
-
-    updateSliderButtons();
-
-    dott();
 });
 
 
 $('.screw_2').click(function () {
-    taksa = 3;
     scrollBlockToLeft();
-
-    var width = parseInt($('.slide_2_wrapper_item').css('width'));
-
-    var p = $('.point_top');
-    // var offset = p.position().left;
-
-    var left = (width / 3) * 2;
-    p.css({
-        left: left,
-        transition: '.2s'
-    });
-
-    updateSliderButtons();
-
-    dott();
 });
 
 
 $('.screw_3').click(function () {
-    taksa = 4;
     scrollBlockToLeft();
-
-    var width = parseInt($('.slide_2_wrapper_item').css('width'));
-
-    var p = $('.point_top');
-    // var offset = p.position().left;
-
-    var left = (width);
-    p.css({
-        left: left,
-        transition: '.2s'
-    });
-
-    updateSliderButtons();
-
-    dott();
 });
 
 ////точки слева для перехода по слайдам
@@ -539,21 +316,6 @@ $('.top_to_2').click(function () {
 });
 
 
-// Итоговая сумма на первой странице
-$('.container_main_item').click(function () {
-    if ($(this).hasClass('orange')) {
-        if ($(this).hasClass('one')) {
-            $('.result_num span').text(cost_1);
-        } else if ($(this).hasClass('three')) {
-            $('.result_num span').text(cost_3);
-        } else if ($(this).hasClass('four')) {
-            $('.result_num span').text(cost_4);
-        }
-    }
-});
-
-
-
 //шестеренка и переход на страницу с предустановленными программами
 $('.screw_1').on('click', function () {
 
@@ -589,31 +351,8 @@ $('.container_main_item_turn_block').click(function () {
     }
 });
 
-    
-
-$('.container_main_item').click(function () {
-    if (!$(this).hasClass('orange')) {
-        $('.taksa_1, .taksa_2, .taksa_3, .taksa_4').removeClass('orange');
-        $(this).addClass('orange');
-    }
-
-    if ($(this).hasClass('taksa_1')) {
-        taksa = 1;
-    } else if ($(this).hasClass('taksa_2')) {
-        taksa = 2;
-    } else if ($(this).hasClass('taksa_3')) {
-        taksa = 3;
-    } else if ($(this).hasClass('taksa_4')) {
-        taksa = 4;
-    }
-
-    initDataSlide();
-});
-
-
-
 // somethings 
-
+/*
 $('.taksa_index_conteiner_item_box_reserve_toggle').click(function () {
     $('.taksa_index_conteiner_item_box_reserve_dropdown').toggle(200);
 });
@@ -624,21 +363,13 @@ $('.taksa_index_conteiner_item_box_moneyback_toggle').click(function () {
 
 $('.taksa_index_conteiner_item_box_receipt_toggle').click(function () {
     $('.taksa_index_conteiner_item_box_receipt_dropdown').toggle(200);
-});
+});*/
 
 
 $('.create_taksa_btn').click( function () {
     $('html').animate({scrollTop:0},400);
 })
 
-
-
-
-
-/////////////////переход на страницу настроек
-$('.create_taksa_btn, .point_first, .point_second, .point_third, .point_fourth, .screw_1, .screw_2, .screw_3, .point_left_1, .point_left_2, .point_left_3, .point_left_4').click(function () {
-    initDataSlide();
-});
 
 function getVal(valName, numVal) {
     return eval(valName + numVal);
@@ -686,7 +417,6 @@ $('.cancel_btn').click(function () {
         });
     });
 
-    initBaseVars();
     
     $('.green_btn').click(); // ?? 
 
@@ -708,24 +438,6 @@ $('.cancel_btn').click(function () {
     return;
 });
 
-// Constants
-var DEFAULT_TAKSA = 3;
-
-var GREEN = 1;
-var ORANGE = 2;
-var PURPLE = 3;
-
-
-
-var basePriceArray = [];
-//такса 1-4
-var cost_1, month_1, limit_1, green_percent_1, orange_percent_1, purple_percent_1;
-var cost_2, month_2, limit_2, green_percent_2, orange_percent_2, purple_percent_2;
-var cost_3, month_3, limit_3, green_percent_3, orange_percent_3, purple_percent_3;
-var cost_4, month_4, limit_4, green_percent_4, orange_percent_4, purple_percent_4;
-
-//доп поля
-var counterArray = [];
 
 //страница настроек
 var pos = 3,
@@ -755,111 +467,6 @@ var percentReserve1 = false;
 var percentReserve2 = false;
 
 
-// Активация/деактивация конфигуратора
-///////////////////////////
-// $('.slide_2_wrapper_buttons_btn_3').click( function () {
-//     $('<div class="gray"></div>').appendTo('.slide_2_wrapper');
-//     $('.slide_2_wrapper_item').css('filter', 'grayscale(100%)');
-// });
-// $('.slide_2_wrapper_buttons_btn_1').click( function () {
-//     $('.gray').remove();
-//     $('.slide_2_wrapper_item').css('filter', 'none')
-// });
-//////////////////////////////
-
-/* functions */
-
-// Изменение процентов по всем Таксам и Цветам на +/- значение ValueChange
-function updatePercentValues(valueChange) {
-    var percent;
-
-    for (var iColor = 1 ; iColor <= 3 ; iColor ++) {
-        for (var iTaksa = 1 ; iTaksa <= 4; iTaksa ++) {
-
-            percent = eval(intColorArray[iColor] + '_percent_' + iTaksa);
-            percent = percent + valueChange;
-            strings = intColorArray[iColor] + '_percent_' + iTaksa;
-            eval(strings +' = percent');
-            
-        }
-    }
-    recalculateCounter();
-}
-
-function getPercent (colorPercent) {
-    return eval(intColorArray[colorPercent] + '_percent_' + taksa);
-}
-
-// Обновление Баннера на Главной странице
-function refreshBannerSlide() {
-    var bms = $('.banner_main_slider');
-    bms.find('.base_price_' + color + ' span').text(basePriceArray[color]);
-    bms.find('.banner_main_slider_' + color + '_content_percent span').text(getVal(intColorArray[color]+'_percent_', taksa));
-    bms.find('.banner_main_slider_' + color + '_content_price span').text(Math.floor(basePriceArray[color] * ((100 - getVal(intColorArray[color]+'_percent_', taksa)) / 100)));
-}
-
-// Обновление Кнопок на Слайдере Settings
-function updateSliderButtons() {
-    taksaStep = taksa;
-    month = $('.box .box_settings .slide_2_wrapper_item_button .value_month');
-    price = $('.box .box_settings .slide_2_wrapper_item_button .value_price');
-    summ = $('.box .box_settings .slide_2_wrapper_item_button .value_summ');
-    month.text(eval('month_' + taksa));
-    price.text(eval('cost_' + taksa) + ' руб.');
-    summ.text(eval('limit_' + taksa) +  ' руб.');
-}
-
-
-
-// Инициация базовых переменных
-function initBaseVars() {
-
-    taksa = DEFAULT_TAKSA; // 6 метров
-    color = 1; // зеленый
-
-    
-    basePriceArray[1] = parseFloat($('.store').find('.store_price_1').text());
-    basePriceArray[2] = parseFloat($('.store').find('.store_price_2').text());
-    basePriceArray[3] = parseFloat($('.store').find('.store_price_3').text());
-    basePriceArray[4] = parseFloat($('.store').find('.store_price_4').text());
-
-//такса 1
-    cost_1 = parseInt($('.one').find('.col_cost span').text());
-    month_1 = $('.one').find('.col_month').text();
-    limit_1 = parseInt($('.one').find('.col_limit span').text());
-    green_percent_1 = parseFloat($('.one').find('.percent_col_green span').text().replace(',', '.'));
-    orange_percent_1 = parseFloat($('.one').find('.percent_col_orange span').text().replace(',', '.'));
-    purple_percent_1 = parseFloat($('.one').find('.percent_col_purple span').text().replace(',', '.'));
-
-//такса 2
-    cost_2 = parseInt($('.two').find('.col_cost span').text());
-    month_2 = $('.two').find('.col_month').text();
-    limit_2 = parseInt($('.two').find('.col_limit span').text());
-    green_percent_2 = parseFloat($('.two').find('.percent_col_green span').text().replace(',', '.'));
-    orange_percent_2 = parseFloat($('.two').find('.percent_col_orange span').text().replace(',', '.'));
-    purple_percent_2 = parseFloat($('.two').find('.percent_col_purple span').text().replace(',', '.'));
-
-//такса 3
-    cost_3 = parseInt($('.three').find('.col_cost span').text());
-    month_3 = $('.three').find('.col_month').text();
-    limit_3 = parseInt($('.three').find('.col_limit span').text());
-    green_percent_3 = parseFloat($('.three').find('.percent_col_green span').text().replace(',', '.'));
-    orange_percent_3 = parseFloat($('.three').find('.percent_col_orange span').text().replace(',', '.'));
-    purple_percent_3 = parseFloat($('.three').find('.percent_col_purple span').text().replace(',', '.'));
-
-//такса 4
-    cost_4 = parseInt($('.four').find('.col_cost span').text());
-    month_4 = $('.four').find('.col_month').text();
-    limit_4 = parseInt($('.four').find('.col_limit span').text());
-    green_percent_4 = parseFloat($('.four').find('.percent_col_green span').text().replace(',', '.'));
-    orange_percent_4 = parseFloat($('.four').find('.percent_col_orange span').text().replace(',', '.'));
-    purple_percent_4 = parseFloat($('.four').find('.percent_col_purple span').text().replace(',', '.'));
-
-    recalculateCounter();    
-
-    initDataSlide();
-
-}
 
 function recalculateCounter () {
     counterArray[1] = (limit_1 * purple_percent_1) / 100;
@@ -875,15 +482,6 @@ function updateSpansAndPercents() {
         $('.green_percent').text(getPercent(1));
         $('.orange_percent').text(getPercent(2));
         $('.purple_percent').text(getPercent(3));
-}
-
-function initDataSlide () {
-    // Обновление Баннера на Главной странице
-    /*refreshBannerSlide();*/
-    // Обновление % по цветам 
-    updateSpansAndPercents();
-    // Обновление Кнопок на Слайдере Settings
-    updateSliderButtons();
 }
 
 function scrollBlockToLeft() {
--- angular/mainPageModule/mainPageModule.js
+++ angular/mainPageModule/mainPageModule.js
@@ -6,6 +6,10 @@ function($scope, $rootScope, getRequest, $timeout){
 	$scope.current;
 	$scope.currentDisc;
 	$scope.vip = false;
+	
+	$scope.moneyBackOptions;
+	$scope.resPeriodOptions;
+	$scope.payTypeOptions;
 
 	$scope.bonuseType = {
 		"one": false,
@@ -18,20 +22,65 @@ function($scope, $rootScope, getRequest, $timeout){
 		bike: 30000,
 		tv: 40000,
 	};
+
 	$scope.newCost = {
 		phone: 50000,
 		bike: 30000,
 		tv: 40000,
 	};
 
+	$scope.settingData = {
+		overPercent: 0,
+		currentMoneyBack: 0,
+		currentResPeriod: 0,
+		currentPayType: 0,
+	};
+
+	$scope.benefit;
+
 	$scope.getNewCost = function(){
 		if ($scope.currentDisc) {		
 			$scope.newCost = {
-				phone: $scope.oldCost.phone-($scope.oldCost.phone/100*$scope.currentDisc),
-				bike: $scope.oldCost.bike-($scope.oldCost.bike/100*$scope.currentDisc),
-				tv: $scope.oldCost.tv-($scope.oldCost.tv/100*$scope.currentDisc),
+				phone: $scope.oldCost.phone-($scope.oldCost.phone/100*($scope.current.loyaltyTariff.greenDiscount+$scope.settingData.overPercent)),
+				bike: $scope.oldCost.bike-($scope.oldCost.bike/100*($scope.current.loyaltyTariff.redDiscount+$scope.settingData.overPercent)),
+				tv: $scope.oldCost.tv-($scope.oldCost.tv/100*($scope.current.loyaltyTariff.yellowDiscount+$scope.settingData.overPercent)),
 			};	
 		}
+	}
+
+	$scope.initSettingSide = function(){
+		$scope.getDefoltSettingValue();
+		$scope.allContent.offers.forEach(function(el, i){
+			if (el.name == $scope.current.name) {
+				$('.point_top').css('left', $scope.getSettingPosition(i, $scope.allContent.offers.length))
+			}
+		})
+	}
+	$scope.getDefoltSettingValue = function(){
+		$scope.settingData = {
+			overPercent: 0,
+			currentMoneyBack: 0,
+			currentResPeriod: 0,
+			currentPayType: 0,
+		};					
+		$scope.refreshPoints($scope.payTypeOptions, '.reserve_4');	
+		$scope.refreshPoints($scope.resPeriodOptions, '.reserve_2');
+		$scope.refreshPoints($scope.moneyBackOptions, '.reserve_3');	
+	}
+
+	$scope.refreshPoints = function(data, point){
+		data.values.sort(function(prec, next){
+			return prec.optionDiscount > next.optionDiscount;
+		})
+		data.values.forEach(function(el, i){
+			if(el.optionDiscount == 0){
+				console.log(i)
+				console.log(data.values.length)
+				console.log(el.optionDiscount)
+				console.log($scope.getSettingOverPosition(i, data.values.length))
+				$(point).css('left', $scope.getSettingOverPosition(i, data.values.length));
+			}
+		})
 	}
 
 	$scope.findForCurrent = function(data){
@@ -51,16 +100,34 @@ function($scope, $rootScope, getRequest, $timeout){
 		})
 		return offers;
 	}
+
+	$scope.getOptions = function(data){
+		$scope.allContent.options.forEach(function(el, i){
+			switch(el.optionName){
+				case "Период резервирования":
+					$scope.resPeriodOptions = el;
+				break;
+				case "Способ оплаты":
+					$scope.payTypeOptions = el;
+				break;
+				case "MONEYBACK":
+					$scope.moneyBackOptions = el;
+				break;
+			}
+		})
+	}
+
 	getRequest.getContent().then(function(res){
 		console.log(res);
 		$scope.allContent = res.data;
 		$scope.offers = $scope.findForCurrent(res.data.offers);
 		$scope.current = $scope.offers[1];
 		$scope.currentDisc = $scope.current.loyaltyTariff.greenDiscount;
+		console.log($scope.current);
       	$scope.addScript();
       	$scope.getNewCost();
-		console.log($scope.offers);
-		console.log($scope.current);
+      	$scope.getBenefit();
+      	$scope.getOptions();
 	}, function(){
 		$('.box').css('height', $('.box_greeting').height());
 	});
@@ -75,6 +142,8 @@ function($scope, $rootScope, getRequest, $timeout){
 
 	$scope.changeCurrentTariff = function(number){
 		$scope.current = $scope.offers[number];
+		$scope.getNewCost();
+		$scope.getBenefit();
 	}
 
 	$scope.getCurrentImg = function(){		
@@ -118,7 +187,6 @@ function($scope, $rootScope, getRequest, $timeout){
 	};
 
 	$scope.changeBonuseType = function(id){	
-
 		$scope.bonuseType = {
 			"one": false,
 			"two": false,
@@ -127,8 +195,70 @@ function($scope, $rootScope, getRequest, $timeout){
 		$scope.bonuseType[id] = true;
 	};
 	$scope.changeDiscont = function(id){	
-		$scope.currentDisc = $scope.current.loyaltyTariff[id];
-		$scope.getNewCost();
+		if ($scope.current) {			
+			$scope.currentDisc = $scope.current.loyaltyTariff[id];
+		}
 	};
 
+	$scope.changeCurrentKitInPeriod = function(kit){	
+		$scope.current = kit;
+		$scope.getNewCost();
+		$scope.getBenefit();
+		$scope.getDefoltSettingValue();
+	};
+
+	$scope.getSettingPosition = function(i, length){
+		if (i==0) {
+			return '0%';
+		}else if(i==length-1){
+			return '99.9%';
+		}else{
+			if ((i+1) <= (length/2)) {
+				return (((100/(length-1))*(i))-5*i)+"%";
+			}else{
+				return (((100/(length-1))*(i))+5*(length-i-1))+"%";
+			}
+		}
+	};
+
+	$scope.getSettingOverPosition = function(i, length, obj, point){
+		var pos;
+		if (i==0) {
+			pos = '0%';
+		}else if(i==length-1){
+			pos = '99.9%';
+		}else if(i==Math.floor((length-1)/2)){
+			pos = '50%';
+		}else{
+			if ((i+1) <= (length/2)) {
+				pos = (((100/(length-1))*(i))-5*i)+"%";
+			}else{
+				pos = (((100/(length-1))*(i))+5*(length-i-1))+"%";
+			}
+		}
+		return pos;
+	};
+
+	$scope.getBenefit = function(){
+		$scope.benefit = $scope.current.loyaltyTariff.maxVolume/100*($scope.current.loyaltyTariff.redDiscount+$scope.settingData.overPercent)		
+	}
+
+	$scope.changeResPeriod = function(res, type){
+		$scope.settingData.overPercent -= $scope.settingData[type];
+		$scope.settingData.overPercent += res.optionDiscount;
+		$scope.settingData[type] = res.optionDiscount;
+		$scope.getBenefit();
+		$scope.getNewCost();
+	}
+	$scope.getNewPersent = function(pers){
+		return pers + $scope.settingData.overPercent;
+	}
 }])
+
+/*
+	$scope.settingData = {
+		overPercent: 0,
+		currentMoneyBack: 0,
+		currentResPeriod: 0,
+		currentPayType: 0,
+	};*/
