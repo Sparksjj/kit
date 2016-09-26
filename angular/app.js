44434da770c1187758f46e162faaac303f5d6064
sparksjj <e.gursky@mobiles.by>
2016-09-26 10:47:57 +0300

    add odometer and fix style

--- angular/app.js
+++ angular/app.js
@@ -1,4 +1,4 @@
-var app = angular.module("myApp", ['ngRoute', 'mainPageModule']);
+var app = angular.module("myApp", ['ngRoute', 'mainPageModule', 'ngOdometer']);
 
   app.config(function($routeProvider) {
       $routeProvider
